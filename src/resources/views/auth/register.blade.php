10a28719c4ae67db578b5f6d24b67abd493a6be8
MasanaIto <z3heng0326@gmail.com>
2021-02-24 02:05:09 +0900

    会員登録時のバリデーション実行時のエラーメッセージ表示を実装

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'ユーザー登録')
 
 @section('content')
+@include('nav')
 <div class="container">
     <div class="row">
         <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
@@ -11,6 +12,7 @@
                 <div class="card-body text-center">
                     <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
 
+                    @include('error_card_list')
                     <div class="card-text">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
