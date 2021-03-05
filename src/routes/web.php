e5d50cde1855229645dea257dd561176c1bc5272
MasanaIto <z3heng0326@gmail.com>
2021-03-05 16:03:53 +0900

    ログイン機能を作成
    ログインページ作成
    ルート編集
    authエラーメッセージ作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,4 +2,8 @@
 
 Auth::routes();
 Route::get('/', 'LandingController@index');
-Route::get('/home', 'ArticleController@index');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', 'ArticleController@index')->name('articles.index');
+    Route::resource('/articles', 'ArticleController')->except(['index']);
+});
