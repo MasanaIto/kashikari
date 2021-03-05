568876eccfca70a3003b1ee483ec6afa0baae927
MasanaIto <z3heng0326@gmail.com>
2021-03-05 18:33:28 +0900

    ArticleRequestクラスを実装
    Articleクラス プロパティ定義を追記

--- src/app/Article.php
+++ src/app/Article.php
@@ -7,6 +7,31 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
+    /**
+     * @var string
+     */
+    private $article_name;
+
+    /**
+     * @var int
+     */
+    private $lender_id;
+
+    /**
+     * @var int
+     */
+    private $borrower_id;
+
+    /**
+     * @var date
+     */
+    private $deadline;
+
+    /**
+     * @var string
+     */
+    private $description;
+
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
